c7862ef7e3d058a78f868a15bb2978d0144fa530
jancarlohendriks <jancarlohendriks@hotmail.com>
2021-12-15 00:15:25 +0100

    minor fixes

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-let t;
+let t = 0;
 let mouseX = 0;
 let mouseY = 0;
-const MIN_SIZE = 10;
-const MAX_SIZE = 50;
+const MIN_SIZE = 1;
+const MAX_SIZE = 20;
 const canvas = document.getElementById('output')
 const ctx = canvas.getContext('2d')
 const imgXtra = new Image();
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-imgXtra.src = "https://images.unsplash.com/photo-1529943247435-a5974e63d6e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80";
-imgXtra.crossOrigin="anonymous"
+// imgXtra.src = "https://images.unsplash.com/photo-1529943247435-a5974e63d6e4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1920&q=80";
+imgXtra.src = "bg.jpg";
+// imgXtra.crossOrigin="anonymous"
 
-let isActive = false;
+let isDrawing = false;
 
 document.onpointerdown = (e) => {
-  isActive = true
+	i = 0
+  isDrawing = true
+	timeInterval = setInterval(function() {
+		if (isDrawing) {
+			i += 1, console.log(i)
+		}
+	}, 1);
 }
 
 document.onpointerup = (e) => {
-	isActive = false;
-	// downloadImage();
+	isDrawing = false;
+	clearInterval(timeInterval)
+	downloadImage();
 }
 
 // document.onmousemove = (e) => {
@@ -37,35 +45,21 @@ var circle = {
 	x: 0,
 	y: 0,
 	size: 0,
-	// maxSize: 50,
-	// _needsRandomized: false,
 	update: function() {
-		this.x = lerp(this.x, mouseX, 0.045)
-		this.y = lerp(this.y, mouseY, 0.045)
+		this.x = lerp(this.x, mouseX, 0.075)
+		this.y = lerp(this.y, mouseY, 0.075)
 		this.size = Math.abs(Math.round(Math.sin(t) * MAX_SIZE));
-		// this.size = MAX_SIZE;
-		// if (this.size < 2) {
-		// 	if (this._needsRandomized) {
-		// 		this.randomize();
-		// 		this._needsRandomized = false;
-		// 	}
-		// } else {
-		// 	this._needsRandomized = true;
-		// }
 	},
-	// randomize: function() {
-	// 	this.x = getRndInt(50, canvas.width - 50);
-	// 	this.y = getRndInt(50, canvas.height - 50);
-	// 	this.maxSize = getRndInt(MIN_SIZE, MAX_SIZE);
-	// },
 	draw: function() {
-		// ctx.moveTo(this.x, this.y);
 		// ctx.beginPath();
+		// ctx.moveTo(this.x, this.y);
+		// ctx.lineTo(this.x + 20, this.y);
+		// ctx.lineTo(this.x + 20, this.y + 100);
+		// ctx.lineTo(this.x, this.y + 100);
 		// ctx.fillRect(this.x, this.y, 40, 60);
-		// ctx.clip();
-		// ctx.closePath();
-		ctx.ellipse(this.x, this.y, this.size, this.size + 10, Math.PI / 4, 0, 2 * Math.PI);
+		ctx.ellipse(this.x+40, this.y+40, this.size+20, this.size+20, Math.PI / 4, 0, 2 * Math.PI);
 		// ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+		// ctx.closePath();
 	}
 };
 
@@ -80,24 +74,21 @@ function render() {
 	// ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.drawImage(imgXtra, 0, 0);
 	scaleToFill(imgXtra) // ctx.drawImage(imgXtra, 0, 0);
-	// ctx.globalCompositeOperation='source-over';
 	ctx.restore();
+	ctx.globalCompositeOperation='source-over';
 }
 
 let timeInterval;
+let i = 10;
 
 function loop() {
 	requestAnimationFrame(loop);
 	circle.update()
-	// t = 0
-	if (isActive) {
-		// t = 0.001
-		// timeInterval = setInterval(function(){ t = t+1 }, 1000);
-		t = 0.001 * Date.now();
-		// console.log(t)
+	if (isDrawing) {
+		t = 0.01 * i;
+		// t = 0.001 * Date.now();
 		render();
 	}
-	// clearInterval(timeInterval)
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -114,19 +105,6 @@ function scaleToFill(img){
 	var y = (canvas.height / 2) - (img.height / 2) * scale;
 	ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
 }
-
-function getRndInt(min, max) {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// var scaleX = window.innerWidth / canvas.width;
-// var scaleY = window.innerHeight / canvas.height;
-
-// var scaleToFit = Math.min(scaleX, scaleY);
-// var scaleToCover = Math.max(scaleX, scaleY);
-
-// canvas.style.transformOrigin = '0 0'; //scale from top left: ;
-// canvas.style.transform = 'scale(' + scaleToCover + ')';
 
 function lerp (start, end, amt){
   return (1-amt)*start+amt*end
